473b1bdade5b6878a02e627c6db5df4449255a8f
Jatsekku <jatsekku2@gmail.com>
2024-05-27 18:54:26 +0000

    [Erase Your Darlings] Add ZFS-based EYD

--- common/base/default.nix
+++ common/base/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./bootloader.nix
+    ./erase_darlings.nix
     ./i18n.nix
     ./nix.nix
     ./packages.nix
---
+++ common/base/erase_darlings.nix
@@ -0,0 +1,7 @@
+{ pkgs, lib, ... }:
+
+{
+    boot.initrd.postDeviceCommands = lib.mkAfter ''
+        zfs rollback -r rpool/root@blank
+    '';
+}
