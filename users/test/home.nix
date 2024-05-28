fea6b37fe17f8c4018d9c6083a10fe05a5800ab8
Jatsekku <jatsekku2@gmail.com>
2024-05-28 19:12:38 +0000

    [chromium] @vm-test Add chromium browser

--- users/test/home.nix
+++ users/test/home.nix
@@ -5,6 +5,11 @@
     home.homeDirectory = "/home/test";
     home.stateVersion = "23.11";
 
+    # User-level packages
+    home.packages = with pkgs; [
+        chromium
+    ];
+
     # Let home Manager install and manage itself.
     programs.home-manager.enable = true;
 }
