1bba67c125c7a0c7364a3954034747ec5a2c5e45
Jatsekku <jatsekku2@gmail.com>
2024-06-24 20:03:14 +0200

    [virtualisation] Add common virtualisation config

---
+++ common/virtualization.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+    virtualisation = {
+        libvirtd = {
+            enable = true;
+            qemu.runAsRoot = true;
+        };
+    };
+
+    environment.systemPackages = with pkgs; [
+        qemu_kvm
+        qemu
+    ];
+
+    programs.virt-manager.enable = true;
+}
