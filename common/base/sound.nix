a375095c9fd931dad5be9a927cda26e9079d0778
Jatsekku <jatsekku2@gmail.com>
2024-10-01 19:12:37 +0200

    [sound] Remove unused property

--- common/base/sound.nix
+++ common/base/sound.nix
@@ -9,9 +9,4 @@
     	alsa.support32Bit = true;
     	jack.enable = true;
   	};
-
-	# Fix for failing alsa-store.service
-	# https://github.com/NixOS/nixpkgs/issues/319809
-	# https://github.com/ryan4yin/nix-config/blob/main/modules/nixos/desktop/peripherals.nix#L27
-	sound.enable = false;
 }
