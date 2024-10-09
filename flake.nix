79990d193f3276374d544600fd6c9801d3ba2bca
Jatsekku <jatsekku2@gmail.com>
2024-10-10 00:52:56 +0200

    feat: Add top-level flake file

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "NixOS config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    {
+      nixosConfigurations =
+        {
+        };
+
+      # Configure nix fmt
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+    };
+}
