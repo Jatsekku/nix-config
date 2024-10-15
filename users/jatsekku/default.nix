561ebfce0c38471d7dab6de242a8c6f0bab7747b
Jatsekku <jatsekku2@gmail.com>
2024-10-16 00:42:39 +0200

    feat(users): Add jatsekku user

---
+++ users/jatsekku/default.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  outputs,
+  myLib,
+  username,
+  ...
+}:
+{
+  home = {
+    stateVersion = "23.11";
+  };
+
+  myUserManager.${username} = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "libvirtd"
+    ];
+    hashedPassword = "$2b$05$jjs7/brKslnAFeLKxdVCOOhGg1XQvg070w/RBIpE7suo1d807jHri";
+  };
+
+  myHomeManager =
+    {
+    };
+}
