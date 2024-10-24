6ec2604006dc203869e6f5fe2c821a5ea386ce69
Jatsekku <jatsekku2@gmail.com>
2024-10-24 20:14:28 +0200

    feat(anemone): Set timezone and keyboard layout

--- hosts/anemone/default.nix
+++ hosts/anemone/default.nix
@@ -16,4 +16,11 @@
   networking.hostName = "anemone";
   networking.hostId = "0F0F0F01";
   system.stateVersion = "23.11";
+
+  time.timeZone = "Europe/Warsaw";
+  i18n.defaultLocale = "en_US.UTF-8";
+  services.xserver.xkb = {
+    layout = "pl";
+  };
+
 }
