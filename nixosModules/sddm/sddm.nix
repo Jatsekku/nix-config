b97a7b35c8c56198dd50f9b0a6858968045a87e3
Jatsekku <jatsekku2@gmail.com>
2024-10-31 14:40:58 +0100

    feat(sddm): Add sddm-sugar-dark theme

--- nixosModules/sddm/sddm.nix
+++ nixosModules/sddm/sddm.nix
@@ -18,7 +18,14 @@ in
   config = lib.mkIf cfg.enable {
     services.displayManager.sddm = {
       enable = true;
+      theme = "${import ./themes/sddm-sugar-dark.nix { inherit pkgs; }}";
       wayland.enable = true;
     };
+
+    # Needed for SDDM theming
+    environment.systemPackages = with pkgs; [
+      libsForQt5.qt5.qtquickcontrols2
+      libsForQt5.qt5.qtgraphicaleffects
+    ];
   };
 }
