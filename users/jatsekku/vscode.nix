194977b99ef85427b868f4fb0b6616f60e76e065
Jatsekku <jatsekku2@gmail.com>
2024-11-17 16:58:05 +0100

    feat(jatsekku): Add VSCode Docker extension

--- users/jatsekku/vscode.nix
+++ users/jatsekku/vscode.nix
@@ -9,12 +9,17 @@
   ...
 }:
 {
-  programs.vscode.extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    {
-      name = "remote-containers";
-      publisher = "ms-vscode-remote";
-      version = "0.218.0";
-      sha256 = "4Li0sYfHOsJMn5FJtvDTGKoGPcRmoosD9tZ7q9H9DfQ=";
-    }
-  ];
+  programs.vscode.extensions =
+    with pkgs.vscode-extensions;
+    [
+      ms-azuretools.vscode-docker
+    ]
+    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "remote-containers";
+        publisher = "ms-vscode-remote";
+        version = "0.218.0";
+        sha256 = "4Li0sYfHOsJMn5FJtvDTGKoGPcRmoosD9tZ7q9H9DfQ=";
+      }
+    ];
 }
