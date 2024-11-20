55919989e8491a2741742cb8912dc0d6627120a6
Jatsekku <jatsekku2@gmail.com>
2024-11-21 00:21:32 +0100

    feat(homeManagerModules): Disable close confirmation for kitty

--- homeManagerModules/kitty.nix
+++ homeManagerModules/kitty.nix
@@ -14,6 +14,11 @@
   };
 
   config = lib.mkIf config.myHomeManager.kitty.enable {
-    programs.kitty.enable = true;
+    programs.kitty = {
+      enable = true;
+      settings = {
+        confirm_os_window_close = 0;
+      };
+    };
   };
 }
