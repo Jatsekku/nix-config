f601ce80da8cab9c552f73a92826460d8b96631b
Jatsekku <jatsekku2@gmail.com>
2025-02-06 01:57:58 +0100

    feat(iris): Enable waypipe

--- hosts/iris/default.nix
+++ hosts/iris/default.nix
@@ -21,6 +21,7 @@
       zigbee.useZ2M = true;
       zigbee.adapterPort = "/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_a8fe3a457245ed118bfec68f0a86e0b4-if00-port0";
     };
+    waypipe.enable = true;
   };
 
   # JUST FOR TESTING!
