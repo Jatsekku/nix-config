d2bebdd6d2ce1d172e753757bbb606b65ee2d10d
Jatsekku <jatsekku2@gmail.com>
2025-02-22 22:22:46 +0100

    feat(users): Add ewa user

---
+++ users/ewa/default.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  outputs,
+  myLib,
+  username,
+  ...
+}:
+{
+  imports = [
+  ];
+
+  home = {
+    stateVersion = "23.11";
+  };
+
+  myUserManager.${username} = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+    ];
+    hashedPassword = "$2b$05$jjs7/brKslnAFeLKxdVCOOhGg1XQvg070w/RBIpE7suo1d807jHri";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  myHomeManager = {
+    git.enable = true;
+  };
+}
