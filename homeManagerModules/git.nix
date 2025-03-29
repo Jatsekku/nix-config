7baa47f7bedfdbc0ced73345d070f4b59969c24e
Jatsekku <jatsekku2@gmail.com>
2025-03-29 09:05:59 +0100

    feat(homeManagerModules): Extend git module with LHS

--- homeManagerModules/git.nix
+++ homeManagerModules/git.nix
@@ -11,12 +11,14 @@
 {
   options.myHomeManager = {
     git.enable = lib.mkEnableOption "Enable git";
+    git.lfs = lib.mkEnableOption "git LFS";
   };
 
   config = lib.mkIf config.myHomeManager.git.enable {
     programs.git = {
       enable = true;
       userName = username;
+      lfs.enable = config.myHomeManager.git.lfs;
     };
   };
 }
