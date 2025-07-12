a3f58767a33e75464125e5dfd1dd73c30e2947fa
Jatsekku <jatsekku2@gmail.com>
2025-07-13 00:52:00 +0200

    feat(users): Add nari-bot user

---
+++ users/nari-bot/default.nix
@@ -0,0 +1,13 @@
+{
+  ...
+}:
+{
+  home = {
+    stateVersion = "23.11";
+  };
+
+  myUserManager.nari = {
+    isNormalUser = true;
+    hashedPassword = "$2b$05$jjs7/brKslnAFeLKxdVCOOhGg1XQvg070w/RBIpE7suo1d807jHri";
+  };
+}
