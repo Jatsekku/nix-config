be3c0e3ff098e4e93e0b48913628d9fbe90df8e1
Jatsekku <jatsekku2@gmail.com>
2025-10-18 21:41:18 +0200

    sq to anemone hyprland

--- hosts/anemone/hyprland.nix
+++ hosts/anemone/hyprland.nix
@@ -8,7 +8,7 @@
         # "1920x1080@60 -> Resolution: 1920 x 1080, Refreshing 60Hz
         # "0x0" -> Postion (x,y) : 0,0
         # "1" -> No scaling
-        "DP-4,1920x1080@60,0x0,1"
+        "HDMI-A-1,1920x1080@60,0x0,1"
 
         # 2 Screen:
         # "DP-4" -> Display port #4,
@@ -16,12 +16,12 @@
         # "0x0" -> Postion (x,y) : 0,0
         # "1" -> No scaling
         # "transform,2" -> Rotation 180 degrees
-        "DP-5,1920x1080@60,1920x0,1,transform,0"
+        "HDMI-A-2,1920x1080@60,1920x0,1"
       ];
 
       # card1 -> [AMD/ATI] Ellesmere [Radeon RX 470/480/570/570X/580/580X/590]
       # card2 -> [AMD/ATI] Navi 23 [Radeon RX 6600/6600 XT/6600M] (rev c1)
-      env = [ "AQ_DRM_DEVICES,/dev/dri/card1:/dev/dri/card2" ];
+      # env = [ "AQ_DRM_DEVICES,/dev/dri/card1" ];
     };
   };
 }
