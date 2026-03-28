2c28a3ed89b468ea39686fee850530b9262d1709
Jatsekku <jatsekku2@gmail.com>
2026-03-28 17:43:01 +0100

    feat(stachys): Hyprland host settings update

--- hosts/stachys/hyprland.nix
+++ hosts/stachys/hyprland.nix
@@ -9,7 +9,11 @@
         # "0x0" -> Postion (x,y) : 0,0
         # "1.125" -> 1.125 scaling
         "eDP-1,2880x1800@120,0x0,1.125"
+        # When screen is driven by igpu it presents itself as eDP-2
+        "eDP-2,2880x1800@120,0x0,1.2"
       ];
+
+      env = [ "AQ_DRM_DEVICES,/dev/dri/igpu:/dev/dri/dgpu" ];
       source = [
         # source nwg-displays config
         #"~/.config/hypr/monitors.conf"
