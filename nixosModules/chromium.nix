caeef082c659c56d1dd76b338eaba87efb230fbf
Jatsekku <jatsekku2@gmail.com>
2026-06-21 00:37:45 +0900

    tmp chromium pplugins

--- nixosModules/chromium.nix
+++ nixosModules/chromium.nix
@@ -4,29 +4,61 @@
   pkgs,
   ...
 }:
+
+with lib;
 let
   cfg = config.myNixOS.chromium;
 in
 {
   options.myNixOS.chromium = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
+    enable = mkOption {
+      type = types.bool;
       default = false;
-      description = "Enable chromium";
+      description = "Enable chromium browser";
     };
-    withWidevine = lib.mkEnableOption "Widevine support in chromium";
+    withBitwarden = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable Bitwarden support in Chromium";
+    };
+    withDarkReader = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable DarkReader support in Chromium";
+    };
+    withWideVine = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable Widevine support in Chromium";
+    };
+    withUblock = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Add uBlock Origin extension to Chromium";
+    };
   };
 
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = [
-      (
-        if cfg.withWidevine then
-          pkgs.chromium.override {
-            enableWideVine = true;
-          }
-        else
-          pkgs.chromium
-      )
+  config = mkIf cfg.enable {
+
+    # Install chromium with WideVine to play DRM-protected content
+    environment.systemPackages = with pkgs; [
+      (chromium.override { enableWideVine = cfg.withWideVine; })
     ];
+
+    # Set Chromium as the default browser
+    environment.sessionVariables = {
+      BROWSER = "chromium";
+    };
+
+    programs.chromium = {
+      enable = true;
+
+      extensions =
+        [ ]
+        # uBlock Origin
+        ++ optional cfg.withBitwarden "nngceckbapebfimnlniiiahkandclblb"
+        ++ optional cfg.withDarkReader "eimadpbcbfnmbkopoojfekhnkhdbieeh"
+        ++ optional cfg.withUblock "ddkjiahejlhfcafbddmgiahcphecmpfh";
+    };
   };
 }
