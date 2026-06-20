69ac3aa772a6062f62fc06bd4e52c604c22cfd6c
Jatsekku <jatsekku2@gmail.com>
2026-06-21 00:32:14 +0900

    feat(nixosModules): Enable nvidia support in docker

--- nixosModules/virtualization/docker.nix
+++ nixosModules/virtualization/docker.nix
@@ -19,6 +19,13 @@ in
   config = lib.mkIf cfg.enable {
     # Add docker package
     environment.systemPackages = [ pkgs.docker ];
-    virtualisation.docker.enable = true;
+    virtualisation.docker = {
+      enable = true;
+      enableNvidia = lib.mkDefault true;
+    };
+
+    # Enabling nVidia support in docker requires 32bit drivers
+    hardware.graphics.enable32Bit = lib.mkDefault true;
+
   };
 }
