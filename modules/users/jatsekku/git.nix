ab4ea0cbd8f7e295b16b5dc0adbad8d84913e5c0
Jatsekku <Jatsekku2@gmail.com>
2026-08-25 21:34:29 +0200

    feat(jatsekku): Manage git with HM

---
+++ modules/users/jatsekku/git.nix
@@ -0,0 +1,15 @@
+{
+  den.aspects.jatsekku.git = {
+    homeManager = {
+      programs.git = {
+        enable = true;
+        settings = {
+          user = {
+            name = "Jatsekku";
+            email = "Jatsekku2@gmail.com";
+          };
+        };
+      };
+    };
+  };
+}
--- modules/users/jatsekku/jatsekku.nix
+++ modules/users/jatsekku/jatsekku.nix
@@ -2,6 +2,13 @@
 {
   den.aspects.jatsekku = {
     includes = [
+      # TODO: Now I don't know how but specific user aspects
+      # living under it's directory should be included automatically.
+      # It's rather obvious that on this level config
+      # is very specific per user and is not intended to be shared.
+      # Yet I still want to apply separation of concerns and keep settings in different files.
+      den.aspects.jatsekku.git
+
       # Define a user at OS and homeManager levels.
       den.batteries.define-user
       # Set as primary user.
