f6a16efec166dc6aef776ca13cb353c32fb4da35
Jatsekku <Jatsekku2@gmail.com>
2026-08-27 17:17:40 +0200

    feat(virtualization): Add libvirtd aspect

---
+++ modules/virtualization/libvirtd.nix
@@ -0,0 +1,20 @@
+{ lib, ... }:
+let
+  # TODO: Change to actual condition once you decided how to manage it
+  isHostGraphical = true;
+in
+{
+  den.aspects.virtualization.libvirtd = {
+    nixos = { pkgs, ... }: {
+      virtualisation.libvirtd = {
+        enable = true;
+      };
+
+      environment.systemPackages = lib.optionals isHostGraphical (with pkgs; [ virt-manager ]);
+
+      environment.sessionVariables = {
+        LIBVIRT_DEFAULT_URI = "qemu:///system";
+      };
+    };
+  };
+}
