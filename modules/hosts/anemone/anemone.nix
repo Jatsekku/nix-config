f081cb84be5b1d0f306b82e39d7334a05e03b9bf
Jatsekku <Jatsekku2@gmail.com>
2026-08-30 22:18:33 +0200

    feat(anemone): Enable multiple aspects CLEAN IT

--- modules/hosts/anemone/anemone.nix
+++ modules/hosts/anemone/anemone.nix
@@ -5,18 +5,27 @@
 {
   den.aspects.anemone = {
     includes = with den.aspects; [
+      cad.kicad
+
       desktop.niri
       desktop.plasma
       desktop.sddm
 
+      editors.arduino
       editors.nixvim
 
+      gaming.moonlight
       gaming.steam
+      gaming.sunshine
+
+      services.home-assistant
+      services.immich
 
       shells.zsh
 
       (system.disko ./_disko.nix)
       (system.facter ./_facter.json)
+      system.facter-debug
       system.git
       system.grub
       system.locale
@@ -25,7 +34,23 @@
       system.nix
 
       utils.fzf
+      utils.pciutils
+      utils.smart
       utils.tree
+
+      virtualization.libvirtd
+      (virtualization.nixvirt.windows10 {
+        name = "win10-workstation";
+        disks = {
+          path = "/dev/disk/by-id/nvme-WD_BLACK_SN850X_1000GB_24122X800837";
+          serial = "24122X800837";
+        };
+      })
+      (virtualization.nixvirt.linux {
+        name = "alpine";
+        disks = "/home/jatsekku/Downloads/alpine-standard-3.24.1-x86_64.iso";
+      })
+      virtualization.passthrough
 
       web.chromium
     ];
