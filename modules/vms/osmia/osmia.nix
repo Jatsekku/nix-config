cfa43c28e33b61db2d41e438749c53be3b04eef5
Jatsekku <Jatsekku2@gmail.com>
2026-09-23 08:53:54 +0200

    feat(vms): Add osmia VM

---
+++ modules/vms/osmia/osmia.nix
@@ -0,0 +1,74 @@
+{
+  den,
+  ...
+}:
+let
+  name = "osmia-win10";
+  lookingGlassDisplays = [ "virtDisp0" ];
+  storageDiskPath = "/dev/disk/by-id/nvme-WD_BLACK_SN850X_1000GB_24122X800837";
+  memory = 32;
+  vcpu = 24;
+
+  extraConfig = {
+    pm = {
+      suspend-to-mem.enabled = false;
+      suspend-to-disk.enabled = false;
+    };
+
+    devices = {
+      # Network interface
+      interface = {
+        type = "network";
+        source.network = "default";
+        model.type = "e1000e";
+      };
+
+      video = {
+        model = {
+          type = "none";
+        };
+      };
+
+      hostdev = [
+        {
+          type = "pci";
+          managed = true;
+          source.address = {
+            domain = 0;
+            bus = 13;
+            slot = 0;
+            function = 0;
+          };
+        }
+        {
+          type = "pci";
+          managed = true;
+          source.address = {
+            domain = 0;
+            bus = 13;
+            slot = 0;
+            function = 1;
+          };
+        }
+      ];
+    };
+  };
+in
+{
+  den.aspects.vms.osmia = {
+    includes = with den.aspects; [
+      (virtualization.nixvirt.windows10 {
+        inherit
+          name
+          memory
+          vcpu
+          extraConfig
+          lookingGlassDisplays
+          ;
+        disks = [
+          { path = storageDiskPath; }
+        ];
+      })
+    ];
+  };
+}
